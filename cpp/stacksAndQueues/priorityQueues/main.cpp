bfa8577783e70d155b4dfd29194b73e3ee7c33df
JonnerPaz <jonnerpazp@gmail.com>
2026-06-22 11:05:00 -0400

    feat: Add test for priorityQueueWithArray and priorityQueueWithLinkedList

--- cpp/stacksAndQueues/priorityQueues/main.cpp
+++ cpp/stacksAndQueues/priorityQueues/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 #include "../priorityQueues/priorityQueueWithArray.hpp"
+#include "../priorityQueues/priorityQueueWithLinkedList.hpp"
 
 using namespace std;
 
-template <typename T>
-void loopPrioQueue(PriorityQueueWithArray<T>*& prioQueue)
+template <typename Q>
+void loopPrioQueue(Q*& prioQueue)
 {
     while (true) {
         auto data = prioQueue->pop();
@@ -18,8 +19,9 @@ void loopPrioQueue(PriorityQueueWithArray<T>*& prioQueue)
     }
 }
 
-int main()
+void testPriorityQueueWithArray()
 {
+    cout << "=== PriorityQueueWithArray ===" << endl;
     PriorityQueueWithArray<int>* masterList = new PriorityQueueWithArray<int>();
     cout << "Pasando elements a masterlist (por defecto, la 5)" << endl;
     masterList->push(3);
@@ -39,5 +41,39 @@ int main()
     masterList->push(7);     // Prioridad mínima (5)
     cout << "Viendo los datos: " << endl;
     loopPrioQueue(masterList);
+
+    delete masterList;
+}
+
+void testPriorityQueueWithLinkedList()
+{
+    cout << "\n=== PriorityQueueWithLinkedList ===" << endl;
+    PriorityQueueWithLinkedList<int>* masterList = new PriorityQueueWithLinkedList<int>();
+    cout << "Pasando elements a masterlist (por defecto, la 5)" << endl;
+    masterList->push(3);
+    masterList->push(4);
+    masterList->push(5);
+    cout << "Datos pasados a masterList, pero sin prioridad" << endl;
+    cout << "Viendo los datos: " << endl;
+    loopPrioQueue(masterList);
+
+    cout << "Pasando elements a masterList, pero con prioridad" << endl;
+    masterList->push(1, 0);  // Prioridad máxima
+    masterList->push(2, 1);  // Prioridad alta
+    masterList->push(3, 2);  // Prioridad media
+    masterList->push(4, 3);  // Prioridad baja
+    masterList->push(5, 2);  // Prioridad media
+    masterList->push(6, 0);  // Prioridad máxima
+    masterList->push(7);     // Prioridad mínima (5)
+    cout << "Viendo los datos: " << endl;
+    loopPrioQueue(masterList);
+
+    delete masterList;
+}
+
+int main()
+{
+    testPriorityQueueWithArray();
+    testPriorityQueueWithLinkedList();
     return 0;
 }
